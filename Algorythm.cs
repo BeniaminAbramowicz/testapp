0fdc7fcedecff9142ef72fc91304e5c4bfd3dbfb
Beniamin Abramowicz <beniamin.abramowicz@gmail.com>
2020-05-22 12:45:12 +0200

    Add class with static method to count the result and main method running the program

---
+++ Algorythm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Algorythm
+    {
+        public static int CountResult(int h, double w, double b)
+        {
+            double temp = h;
+            int result = 0;
+            if(b == 0 || b == 1 || h <= w)
+            {
+                result = -1;
+                return result;
+            }
+            else
+            {
+                do
+                {
+                    if (temp == h && temp > w)
+                    {
+                        result++;
+                        temp *= b;
+                    }
+                    else
+                    {
+                        result += 2;
+                        temp *= b;
+                    }
+
+                } while (temp >= w);
+
+                return result;
+            } 
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,17 @@ namespace TestApp
     {
         static void Main(string[] args)
         {
+            int h;
+            double b, w = 1.5;
+
+            Console.WriteLine("Wprowadź wysokość h z której zrzucana jest piłka: ");
+            h = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Wprowadź współczynnik b wysokości odbicia piłki - zakres od 0 do 1: ");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Mama zobaczyła piłkę " + Algorythm.CountResult(h, w, b) + " razy");
+            
+            Console.ReadKey();
         }
     }
 }
